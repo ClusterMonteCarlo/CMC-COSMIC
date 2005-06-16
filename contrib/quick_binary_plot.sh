1b50bdcef6afa06a2ed22ba3b9706628d92fbd52
John F. Fregeau <fregeau@gmail.com>
2005-06-16 17:50:45 +0000

    Updated script for binary plot.
    git-svn-id: svn+ssh://svn.wcas.northwestern.edu/var/svn/cmc/trunk@239 46abec54-52ec-0310-8d7b-a975267b85f2

--- contrib/quick_binary_plot.sh
+++ contrib/quick_binary_plot.sh
@@ -6,8 +6,10 @@ xmgrace -pexec "arrange(3,1,0.15,0,0)" \
     -pexec "yaxis label \"M\sb\N/M\sb\N(0), M/M(0)\"" \
     -pexec "xaxis ticklabel off" \
     -graph 1 -block $1.bin.dat -settype xy -bxy 1:13 \
+    -pexec "s0.y = s0.y / 0.25" \
     -graph 1 -block $1.bin.dat -settype xy -bxy 1:14 \
-    -pexec "yaxis label \"E\sbb\N, E\sbs\N [FP units]\"" \
+    -pexec "s1.y = s1.y / 0.25" \
+    -pexec "yaxis label \"E\sbb\N, E\sbs\N [|E\sc\N(0)|]\"" \
     -pexec "xaxis ticklabel off" \
     -graph 2 -log y -block $1.dyn.dat -settype xy -bxy 1:8 \
     -graph 2 -log y -block $1.bin.dat -settype xy -bxy 1:5 \
