11a06d9aacb832b6db44ecd4d17841f7ee2b20bd
Diden <diden@gmail.com>
2006-07-05 18:27:55 +0000

    Graph outputted from make_density_plot now includes title with time in code units.
    git-svn-id: svn+ssh://svn.wcas.northwestern.edu/var/svn/cmc/trunk@276 46abec54-52ec-0310-8d7b-a975267b85f2

--- contrib/density_plot/make_density_plot.sh
+++ contrib/density_plot/make_density_plot.sh
@@ -5,5 +5,6 @@ echo "Extracting 3D density from snapshot..."
 3d_density.rb $1 $2 $3 
 echo "Extracting 2D luminosity/density from snapshot..."
 2d_density.rb $1 $2 $3
+export time=`zcat $1.snap$2.dat.gz | head -n 1 | awk '{print $2}' | cut -d = -f 2`
 echo "Generating plot..."
-quick_density_plot.sh $1 $2
+quick_density_plot.sh $1 $2 $time
