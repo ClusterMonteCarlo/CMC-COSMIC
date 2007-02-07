177cdf5a63904f1ba2b2f3e2413697ca11ab181c
John F. Fregeau <fregeau@gmail.com>
2007-02-07 16:54:20 +0000

    Updated to either bin radially or by number of stars, takes command line options for bin size.
    git-svn-id: svn+ssh://svn.wcas.northwestern.edu/var/svn/cmc/trunk@354 46abec54-52ec-0310-8d7b-a975267b85f2

--- contrib/calc_3ddensity.pl
+++ contrib/calc_3ddensity.pl
@@ -3,23 +3,64 @@
 # This script will calculate, from an output snapshot file, the 3d mass density
 # The script is kind of slow in Perl, but I don't plan on using it much...
 
+use Getopt::Std;
 use strict;
 
 # declare variables and subroutines
-my ($usage, $line, @vals, @r, @m);
+my ($usage, %Options, $line, @vals, @r, @m);
 my ($n, $mtot, $i, $j, $p, $r, $imin);
-my ($imax, $rmin, $rmax, $m, $rho);
+my ($imax, $rmin, $rmax, $m, $rho, $radbin);
+
+# base 10 logarithm
+sub log10 {
+    my $inval = shift(@_);
+    return(log($inval)/log(10));
+}
+
+# default options
+$p = 10;
+$radbin = 0;
 
 # the usage
-$usage = 
-"Usage: $0
-Calculates the 3d mass density profile and writes it to stdout.
-You'll have to cat (or zcat) the snapshot file to stdin.
-";
+$usage = "Calculates 3D density from snapshot file on stdin and writes it stdout.
 
-# wrong number of arguments?
+USAGE:
+  $0 [options...]
+
+OPTIONS:
+  -p <p> : bin by star number and set 1/2 of averaging kernel to this value [$p]
+  -b <b> : bin by radius and use this many bins [$radbin]
+  -h     : display this help text\n";
+
+# get options
+if (!getopts('p:b:h', \%Options)) {
+    die("$usage");
+}
+
+# no required arguments
 if ($#ARGV != -1) {
     die("$usage");
+}
+
+# print help if necessary
+if ($Options{h}) {
+    die("$usage");
+}
+
+# these options are mutually exclusive
+if ($Options{p} && $Options{b}) {
+    die("You must specify only one of -p or -b.\n");
+}
+
+# specified options
+if ($Options{p}) {
+    $p = $Options{p};
+    $radbin = 0;
+}
+
+if ($Options{b}) {
+    $radbin = $Options{b};
+    $p = 0;
 }
 
 # read in snapfile
@@ -46,30 +87,50 @@ for ($i=0; $i<=$n-1; $i++) {
 }
 
 # calculate 3d density by averaging
-$p = 20;
-for ($i=0; $i<=$n-1; $i++) {
-    $r = $r[$i];
-    
-    $imin = $i - $p;
-    if ($imin < 0) {
-	$imin = 0;
+if ($p) {
+    for ($i=0; $i<=$n-1; $i++) {
+	$r = $r[$i];
+	
+	$imin = $i - $p;
+	if ($imin < 0) {
+	    $imin = 0;
+	}
+	
+	$imax = $i + $p;
+	if ($imax > $n-1) {
+	    $imax = $n-1;
+	}
+	
+	$rmin = $r[$imin];
+	$rmax = $r[$imax];
+	
+	# use only half the mass of the stars on the edges
+	$m = $m[$imin]/2.0 + $m[$imax]/2.0;
+	for ($j=$imin+1; $j<=$imax-1; $j++) {
+	    $m += $m[$j];
+	}
+	
+	$rho = $m/(4.0/3.0 * 3.14159265358979312 * ($rmax*$rmax*$rmax - $rmin*$rmin*$rmin));
+	
+	print("$r $rho\n");
     }
-    
-    $imax = $i + $p;
-    if ($imax > $n-1) {
-	$imax = $n-1;
+} elsif ($radbin) {
+    $n = 0;
+    for ($i=0; $i<$radbin; $i++) {
+	$rmin = 10**($i/$radbin * (log10($r[$#r])-log10($r[0])) + log10($r[0]));
+	$rmax = 10**(($i+1)/$radbin * (log10($r[$#r])-log10($r[0])) + log10($r[0]));
+	
+	$m = 0;
+	while ($n <= $#r && $r[$n] <= $rmax) {
+	    $m += $m[$n];
+	    $n++;
+	}
+	
+	$r = 10**(0.5 * (log10($rmin) + log10($rmax)));
+	$rho = $m/(4.0/3.0 * 3.14159265358979312 * ($rmax*$rmax*$rmax - $rmin*$rmin*$rmin));
+	
+	print("$r $rho\n");
     }
-    
-    $rmin = $r[$imin];
-    $rmax = $r[$imax];
-    
-    # use only half the mass of the stars on the edges
-    $m = $m[$imin]/2.0 + $m[$imax]/2.0;
-    for ($j=$imin+1; $j<=$imax-1; $j++) {
-	$m += $m[$j];
-    }
-    
-    $rho = $m/(4.0/3.0 * 3.14159265358979312 * ($rmax*$rmax*$rmax - $rmin*$rmin*$rmin));
-    
-    print("$r $rho\n");
+} else {
+    die("Unknown binning scheme.\n");
 }
