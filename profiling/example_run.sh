c356a6ba234d5e28e7928dcf106d1f34811feb50
John F. Fregeau <fregeau@gmail.com>
2007-04-16 17:18:56 +0000

    Updated.
    git-svn-id: svn+ssh://svn.wcas.northwestern.edu/var/svn/cmc/trunk@367 46abec54-52ec-0310-8d7b-a975267b85f2

--- profiling/example_run.sh
+++ profiling/example_run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 ../cmc -d pl_n1e5_fb0.cmc pl_n1e5_fb0.out > pl_n1e5_fb0.stdout 2> pl_n1e5_fb0.stderr
+../cmc -d pl_n1e5_fb1.cmc pl_n1e5_fb1.out > pl_n1e5_fb1.stdout 2> pl_n1e5_fb1.stderr
