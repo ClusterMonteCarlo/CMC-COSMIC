fa43b829cfe0c753a0030dc8fc72e1640574b377
Stefan Umbreit <s-umbreit@northwestern.edu>
2007-05-03 01:04:37 +0000

    BugFix: It can happen that if r is much smaller than star[1].r, sindex.min becomes larger than 1. This might be because the power-law that assignes grid points to radii and vice versa is only strictly increasing for r>star[1].r and might have a minimum below that (weird!).
    git-svn-id: svn+ssh://svn.wcas.northwestern.edu/var/svn/cmc/trunk@379 46abec54-52ec-0310-8d7b-a975267b85f2

--- cmc_search_grid.c
+++ cmc_search_grid.c
@@ -141,6 +141,9 @@ search_grid_get_interval(struct Search_Grid *grid, double r) {
    * largest or smallest particle radius*/ 
   sindex.max++;
   sindex.min--;
+  
+  /* This prevents that min becomes larger than 1 if r<star[1].r (weird!!)*/
+  if (r<star[1].r) sindex.min= 1
 
   return(sindex);
 };
