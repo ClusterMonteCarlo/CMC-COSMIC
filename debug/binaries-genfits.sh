75ed0feb5b89f6d7702d79aecc2f9adeaa4f83ee
John F. Fregeau <fregeau@gmail.com>
2007-08-13 16:32:18 +0000

    Updated for more reasonable number of stars.
    git-svn-id: svn+ssh://svn.wcas.northwestern.edu/var/svn/cmc/branches/cmc-springcleaning@488 46abec54-52ec-0310-8d7b-a975267b85f2

--- debug/binaries-genfits.sh
+++ debug/binaries-genfits.sh
@@ -1,5 +1,5 @@
-cmc_mkking -w 5 -N 500000 -o /tmp/test.fits
+cmc_mkking -w 5 -N 100000 -o /tmp/test.fits
 cmc_setimf -i /tmp/test.fits -o /tmp/test2.fits -I 0 -m 0.05 -M 5 && rm /tmp/test.fits
 cmc_setunits -i /tmp/test2.fits -o /tmp/test3.fits -R 3.5 && rm /tmp/test2.fits
 cmc_setstellar -i /tmp/test3.fits -o /tmp/test4.fits && rm /tmp/test3.fits
-cmc_addbinaries -i /tmp/test4.fits -o binaries.fits -N 250000 -l 0 && rm /tmp/test4.fits
+cmc_addbinaries -i /tmp/test4.fits -o binaries.fits -N 10000 -l 0 && rm /tmp/test4.fits
