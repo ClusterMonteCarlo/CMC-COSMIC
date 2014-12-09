ea644c952cff62acb5b89f79aa7c7407d08941a8
Carl Rodriguez <carllouisrodriguez@gmail.com>
2014-12-09 20:39:06 +0000

    updated cmc_nbody2fits to accept binary files
    git-svn-id: svn+ssh://svn.wcas.northwestern.edu/var/svn/cmc/branches/cmc-mpi@1099 46abec54-52ec-0310-8d7b-a975267b85f2

--- utils/ascii2fits/nbody2fits.c
+++ utils/ascii2fits/nbody2fits.c
@@ -15,7 +15,7 @@
 
 /* default parameters */
 #define RMAX 300.0
-#define NSTAR 100000UL
+#define NSTAR 10000000UL
 #define OUTFILE_FORMAT "%s.fits"
 #define SEED 0UL
 
@@ -25,6 +25,7 @@
 #define DATE "Thu Feb 10 16:58:35 CST 2005"
 
 int debug=0, mtot_given=0, scale_units=1;
+int BINFILE=0;
 
 double Mrtotal, Mtot;
 
@@ -42,11 +43,16 @@ void print_usage(FILE *stream)
 	fprintf(stream, "\n");
 	fprintf(stream, "OPTIONS:\n");
 	fprintf(stream, "  -o --outfile <outfile> : set name of outfile [<snapshot file>.fits]\n");
+	fprintf(stream, "  -i --infile <singlefile> : set name of infile (singles) [<nbody file>.dat]\n");
+	fprintf(stream, "  -b --binaries <binfile> : set name of infile (binaries) [<binary file>.dat]\n");
         fprintf(stream, "  -m --total-mass        : the total mass of the cluster [calculated from snapshot]\n");
         fprintf(stream, "  -s --no-scaling        : turn off scaling of positions and velocities\n");
 	fprintf(stream, "  -d --debug             : turn on debugging\n");
 	fprintf(stream, "  -V --version           : print version info\n");
 	fprintf(stream, "  -h --help              : display this help text\n");
+	fprintf(stream, " nbody infile should be of the form 'm x y z vx vy vz'\n"); 
+	fprintf(stream, " binary infile should be of the form 'e a m1 m2 x y z vx vy vz'\n"); 
+	fprintf(stream, " \ta must be in code units (pc/rVir), since scale will not adjust it later\n");
 }
 
 /* print the version */
@@ -70,16 +76,17 @@ void printerror( int status)
     return;
 }
 
-void write_output_file(double *m, double *r, double *vr, double *vt, long int N, char *filename){
-	long i;
+void write_output_file(double *m, double *r, double *vr, double *vt, double *m1, double *m2, double *a, double *e, double *binind, long int N, long int NBIN, char *filename){
+	long i,j=1;
 	cmc_fits_data_t cfd;
 	
 	cfd.NOBJ = N;
-	cfd.NBINARY = 0;
+	cfd.NBINARY = NBIN;
 	cfd.Mclus = Mtot;
 	cfd.Rvir = 1.0;
 	cfd.Rtid = 1.0e6;
 	cfd.Z = 0.02;
+    fprintf(stdout, "%ld %ld\n",N,NBIN);
 
 	cmc_malloc_fits_data_t(&cfd);
 
@@ -91,7 +98,22 @@ void write_output_file(double *m, double *r, double *vr, double *vt, long int N,
 		cfd.obj_r[i] = r[i];
 		cfd.obj_vr[i] = vr[i];
 		cfd.obj_vt[i] = vt[i];
-		cfd.obj_binind[i] = 0;
+        //fprintf(stderr, "%ld %lg %ld\n",cfd.obj_id[i],cfd.obj_m[i],cfd.obj_binind[i]);
+        if(binind[i] == -1)
+            cfd.obj_binind[i] = 0;
+        else{
+            cfd.obj_id[i] = -1;
+            cfd.obj_binind[i] = j;
+            cfd.bs_index[j] = i;
+            cfd.bs_m1[j] = m1[(long)binind[i]];
+            cfd.bs_m2[j] = m2[(long)binind[i]];
+            cfd.bs_id1[j] = i;
+            cfd.bs_id2[j] = N+j;
+            cfd.bs_a[j] = a[(long)binind[i]];
+            cfd.bs_e[j] = e[(long)binind[i]];
+            //fprintf(stderr, "%lg %lg %lg\n",cfd.obj_m[i],cfd.bs_m1[j],cfd.bs_m2[j]);
+            j++;
+        }
 	}
 	
 	cmc_write_fits_file(&cfd, filename);
@@ -204,7 +226,7 @@ void calculate_mc_vars(double *v3d, double *pos, double *r, double *vr, double *
   *vt= sqrt(*vt);
 };
 
-void sort_radially(double *r, double *vr, double *vt, double *m, size_t N) {
+void sort_radially(double *r, double *vr, double *vt, double *m, double *binind, size_t N) {
   int res;
   gsl_permutation *ind;
 
@@ -224,66 +246,114 @@ void sort_radially(double *r, double *vr, double *vt, double *m, size_t N) {
   res= gsl_permute(ind->data, vr, 1, N+1);
   res= gsl_permute(ind->data, vt, 1, N+1);
   res= gsl_permute(ind->data, m, 1, N+1);
+  if(BINFILE)
+      res= gsl_permute(ind->data, binind, 1, N+1);
 
 };
 
 
-void readNbodySnapshot(const char *filename, double **r, double **vr, double **vt, double **m, unsigned long *N) {
+void readNbodySnapshot(const char *filename, const char *binaries, double **r, double **vr, double **vt, double **m, unsigned long *N, unsigned long *NBIN, double **m1, double **m2, double **a, double **e, double **binind) {
   FILE *inputfile;
-  int items;
-  long i, block, lines;
+  FILE *binariesFile;
+  int itemsSingle, itemsBinary;
+  long i, block, linesSingle, linesBin;
   char *line;
   size_t n;
   double pos[3], vel[3];
 
   line= NULL;
   block= 1000;
-  lines= 0;
+  linesSingle = 0;
+  linesBin = 0;
   n= 1;
-  items= 1;
+  itemsSingle = itemsBinary = 1;
+  if(!BINFILE)
+      itemsBinary = EOF;
 
   *r= (double *) malloc(sizeof(double));
   *vr= (double *) malloc(sizeof(double));
   *vt= (double *) malloc(sizeof(double));
   *m= (double *) malloc(sizeof(double));
+  *m1= (double *) malloc(sizeof(double));
+  *m2= (double *) malloc(sizeof(double));
+  *a= (double *) malloc(sizeof(double));
+  *e= (double *) malloc(sizeof(double));
+  *binind= (double *) malloc(sizeof(double));
   **r= DBL_MIN;
   **vr= **vt= 0;
   **m= 0;
+  **m1=0;
+  **m2=0;
+  **a=0;
+  **e=0;
+  **binind=0;
 
   inputfile= fopen(filename, "r");
+  if(BINFILE)
+      binariesFile = fopen(binaries, "r");
 
   i=1;
-  do {
-    lines+= block;
-    *r= (double *) realloc(*r, sizeof(double)*lines);
-    *vr= (double *) realloc(*vr, sizeof(double)*lines);
-    *vt= (double *) realloc(*vt, sizeof(double)*lines);
-    *m= (double *) realloc(*m, sizeof(double)*lines);
-    for (; items!=EOF && i<lines-1; i++) {
-      items=fscanf(inputfile, 
+  int bin=1;
+  while ((itemsSingle!=EOF) || (itemsBinary!=EOF)){
+    linesSingle+= block;
+    linesBin+= block;
+    *r= (double *) realloc(*r, sizeof(double)*linesSingle);
+    *vr= (double *) realloc(*vr, sizeof(double)*linesSingle);
+    *vt= (double *) realloc(*vt, sizeof(double)*linesSingle);
+    *m= (double *) realloc(*m, sizeof(double)*linesSingle);
+    *m1= (double *) realloc(*m1, sizeof(double)*linesBin);
+    *m2= (double *) realloc(*m2, sizeof(double)*linesBin);
+    *a= (double *) realloc(*a, sizeof(double)*linesBin);
+    *e= (double *) realloc(*e, sizeof(double)*linesBin);
+    *binind= (double *) realloc(*binind, sizeof(double)*linesSingle);
+    for (; itemsSingle!=EOF && i<linesSingle; i++) {
+      itemsSingle=fscanf(inputfile, 
           "%lf %lf %lf %lf %lf %lf %lf\n",
           &(*m)[i], &pos[0], &pos[1], &pos[2], &vel[0], &vel[1], &vel[2]);
+      if(itemsSingle==EOF){
+          i--;
+          continue;
+        }
       calculate_mc_vars(vel, pos, &(*r)[i], &(*vr)[i], &(*vt)[i]);
-    };
-  } while (items!=EOF);
+      (*binind)[i]=-1;
+    }
+    for (; itemsBinary!=EOF && i<linesBin; i++) {
+      itemsBinary=fscanf(binariesFile, 
+          "%lf %lf %lf %lf %lf %lf %lf %lf %lf %lf\n",
+          &(*e)[bin], &(*a)[bin], &(*m1)[bin], &(*m2)[bin], &pos[0], &pos[1], &pos[2], &vel[0], &vel[1], &vel[2]);
+      (*m)[i] = (*m1)[bin]+(*m2)[bin];
+      calculate_mc_vars(vel, pos, &(*r)[i], &(*vr)[i], &(*vt)[i]);
+      if(itemsBinary==EOF){
+          i--;
+          continue;
+        }
+      (*binind)[i]=bin;
+        bin++;
+    }
+  } 
 
-  *N= i-2;
+  *N= i-1;
+  *NBIN = bin-1;
   (*r)[*N+1]= LARGE_DISTANCE;
   (*vr)[*N+1]= (*vt)[*N+1]= 0.;
   (*m)[*N+1]= 0.;
-  sort_radially(*r, *vr, *vt, *m, *N);
+  (*binind)[*N+1]=0;
+  sort_radially(*r, *vr, *vt, *m, *binind, *N);
 }
 
 int main(int argc, char *argv[]){
-	double *r, *vr, *vt, *m;
+	double *r, *vr, *vt, *m, *m1, *m2, *a, *e, *binind;
 	double rmax=RMAX;
 	double rcr = 1.0/(6.0*(PI/32.0));
-	unsigned long int N=NSTAR, seed=SEED;
+	unsigned long int N=NSTAR, NBIN=NSTAR, seed=SEED;
 	char filename[1024]; 
-        char *input;
+        char input[1024];
+        char binaries[1024];
 	int i;
-	const char *short_opts = "so:m:dVh";
+	const char *short_opts = "si:b:o:m:dVh";
 	const struct option long_opts[] = {
+		{"input", required_argument, NULL, 'i'},
+		{"binaries", required_argument, NULL, 'b'},
 		{"outfile", required_argument, NULL, 'o'},
                 {"total-mass", required_argument, NULL, 'm'},
                 {"no-scaling", required_argument, NULL, 's'},
@@ -293,12 +363,19 @@ int main(int argc, char *argv[]){
 		{NULL, 0, NULL, 0}
 	};
 
-        r= vr= vt= m= NULL;
+        r= vr= vt= m= m1= m2= a= e= binind= NULL;
 
 	filename[0] = 0;	/* setting a meaningless default name */
 
 	while ((i = getopt_long(argc, argv, short_opts, long_opts, NULL)) != -1) {
 		switch (i) {
+        case 'i':
+			sprintf(input, "%s", optarg);
+            break;
+        case 'b':
+            BINFILE = 1;
+			sprintf(binaries, "%s", optarg);
+            break;
 		case 'o':
 			sprintf(filename, "%s", optarg);
 			break;
@@ -326,12 +403,12 @@ int main(int argc, char *argv[]){
 	}
 	
 	/* check to make sure there was nothing crazy on the command line */
-	if (optind >= argc) {
+	/*if (optind >= argc) {
 		print_usage(stdout);
 		return(EXIT_FAILURE);
-	}
+	}*/
 
-        input= argv[optind];
+        //input= argv[optind];
 	
 	if (filename[0]==0){
 		sprintf(filename, OUTFILE_FORMAT, input);
@@ -340,7 +417,8 @@ int main(int argc, char *argv[]){
 	dprintf("rmax=%g N=%ld filename=%s seed=%ld\n", rmax, N, filename, seed);
 
 	check_for_file(filename);
-        readNbodySnapshot(input, &r, &vr, &vt, &m, &N);
+        readNbodySnapshot(input, binaries, &r, &vr, &vt, &m, &N,&NBIN,&m1,&m2,&a,&e,&binind);
+
         
         if (!mtot_given) {
           for (i=1; i<N+1; i++) {
@@ -357,8 +435,12 @@ int main(int argc, char *argv[]){
         for (i=1; i<N+1; i++) {
           m[i]/= Mtot;
         };
+        for(i=1; i<NBIN+1; i++){
+          m1[i]/= Mtot;
+          m2[i]/= Mtot;
+        }
 
-        write_output_file(m, r, vr, vt, N, filename);
+        write_output_file(m, r, vr, vt, m1,m2,a,e, binind, N,NBIN, filename);
 
 	/*for(i=1; i<=N; i++){
 		printf("%.30e %.30e %.30e\n", 
