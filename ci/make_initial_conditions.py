b5b70d4b8532916c2569ee79f7e017c05c052c19
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2020-10-13 16:46:59 -0500

    Alright a quick thing to run

---
+++ ci/make_initial_conditions.py
@@ -0,0 +1,7 @@
+from cosmic.sample import InitialCMCTable
+
+Singles, Binaries = InitialCMCTable.sampler('cmc', binfrac_model=0.2, primary_model='kroupa01', ecc_model='sana12', porb_model='sana12', cluster_profile='plummer', met=0.014, size=80000, params='Params.ini', gamma=4, r_max=100)
+
+InitialCMCTable.write(Singles, Binaries, filename="input.hdf5")
+
+InitialCMCTable.write(Singles, Binaries, filename="input.fits")
---
+++ ci/run_cmc_tests.sh
@@ -0,0 +1,2 @@
+python make_initial_conditions.py
+mpirun -n 3 ./cmc Params.ini initial
