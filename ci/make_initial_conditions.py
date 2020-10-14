8ad945e139e857625e018297304866b2ef31d252
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2020-10-13 20:21:09 -0500

    More BSE dict

--- ci/make_initial_conditions.py
+++ ci/make_initial_conditions.py
@@ -1,6 +1,6 @@
 from cosmic.sample import InitialCMCTable
 
-Singles, Binaries = InitialCMCTable.sampler('cmc', binfrac_model=0.2, primary_model='kroupa01', ecc_model='sana12', porb_model='sana12', cluster_profile='plummer', met=0.014, size=20000, params='Params.ini', gamma=4, r_max=100)
+Singles, Binaries = InitialCMCTable.sampler('cmc', binfrac_model=0.2, primary_model='kroupa01', ecc_model='sana12', porb_model='sana12', cluster_profile='plummer', met=0.014, size=10000, params='Params.ini', gamma=4, r_max=100)
 
 InitialCMCTable.write(Singles, Binaries, filename="input.hdf5")
 
