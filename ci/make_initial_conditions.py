4e186b8977d7754a8ecb35d9bcbd9de98d4f3fd7
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2020-11-10 14:21:28 -0600

    Updating cosmic ref

--- ci/make_initial_conditions.py
+++ ci/make_initial_conditions.py
@@ -1,7 +1,3 @@
 from cosmic.sample import InitialCMCTable
 
-Singles, Binaries = InitialCMCTable.sampler('cmc', binfrac_model=0.2, primary_model='kroupa01', ecc_model='sana12', porb_model='sana12', cluster_profile='plummer', met=0.014, size=10000, params='Params.ini', gamma=4, r_max=100)
-
-InitialCMCTable.write(Singles, Binaries, filename="input.hdf5")
-
-InitialCMCTable.write(Singles, Binaries, filename="input.fits")
+Singles, Binaries = InitialCMCTable.sampler('cmc', binfrac_model=0.2, primary_model='kroupa01', ecc_model='sana12', porb_model='sana12', cluster_profile='plummer', met=0.014, size=10000, params='Params.ini', gamma=4, r_max=100, qmin=0.1)
