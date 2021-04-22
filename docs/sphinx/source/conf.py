6d40461937fecce26e0825e9f6f0cea7c12d6eda
Carl <carlrodr@andrew.cmu.edu>
2021-04-22 18:57:23 -0400

    better logo

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -135,7 +135,7 @@ html_theme_options = {
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'logo.png' 
+html_logo = 'logo.001.jpeg' 
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16
