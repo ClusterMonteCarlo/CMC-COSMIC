8c41a44e3aad6832486f03c27abae61c7d33b0f5
Carl Rodriguez <carllouisrodriguez@gmail.com>
2021-05-23 13:50:24 -0400

    Update generate_king_profile.py

--- examples/generate_king_profile.py
+++ examples/generate_king_profile.py
@@ -9,9 +9,8 @@ from cosmic.sample import InitialCMCTable
 # See (https://cosmic-popsynth.github.io/COSMIC/runpop/index.html) for more details on the options here
 Singles, Binaries = InitialCMCTable.sampler('cmc', binfrac_model=0.1, primary_model='kroupa01',
                                             ecc_model='thermal', porb_model='log_uniform', qmin=0.1,
-                                            cluster_profile='king', met=0.00017, size=100000,w_0=6,
-                                            params='KingProfile.ini',seed=12345, virial_radius=1,
-                                            tidal_radius = 1e6)
+                                            cluster_profile='king', met=0.00017, zsun=0.017, size=100000,w_0=6,
+                                            ,seed=12345, virial_radius=1,tidal_radius = 1e6)
 
 # Save them to an hdf5 file for CMC with a virial radius of 1pc 
 # and an initial tidal radius of 10^6 virial radii
